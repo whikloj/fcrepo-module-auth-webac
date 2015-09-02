ff5f47e5150bf25174dd77623469c3e8d435d1cd
Jared Whiklo <jwhiklo@gmail.com>
2015-09-02 11:27:24 -0500

    More stuff for @acoburn

--- src/main/java/org/fcrepo/auth/webac/WebACAuthorizationImpl.java
+++ src/main/java/org/fcrepo/auth/webac/WebACAuthorizationImpl.java
@@ -4,6 +4,7 @@ import static com.google.common.collect.Iterators.filter;
 import static org.fcrepo.kernel.modeshape.rdf.ManagedRdf.isManagedTriple;
 
 import java.net.URI;
+import java.util.Collections;
 import java.util.Set;
 import java.util.function.Predicate;
 
@@ -17,6 +18,7 @@ import org.fcrepo.kernel.modeshape.rdf.impl.PropertiesRdfContext;
 import org.fcrepo.kernel.modeshape.rdf.impl.TypeRdfContext;
 
 import com.hp.hpl.jena.graph.Node;
+import com.hp.hpl.jena.graph.NodeFactory;
 import com.hp.hpl.jena.graph.Triple;
 import com.hp.hpl.jena.rdf.model.Resource;
 
@@ -26,13 +28,12 @@ public class WebACAuthorizationImpl implements WebACAuthorization {
 
     IdentifierConverter<Resource, FedoraResource> idTranslator = null;
 
-    RdfStream permissions;
+    RdfStream permissions = new RdfStream();
 
     public WebACAuthorizationImpl(final IdentifierConverter<Resource, FedoraResource> translator,
             final FedoraResource resource) {
         idTranslator = translator;
         final RdfStream childRdf = resource.getTriples(translator, ChildrenRdfContext.class);
-        permissions = new RdfStream();
         for (final Triple t : childRdf.iterable()) {
             permissions.concat(
                     filter(translator.convert((Resource) t.getObject()).getTriples(translator(), TypeRdfContext.class),
@@ -41,35 +42,48 @@ public class WebACAuthorizationImpl implements WebACAuthorization {
                     translator.convert((Resource) t.getObject()).getTriples(translator(), PropertiesRdfContext.class),
                     tripleFilter::test));
         }
+
     }
 
     @Override
     public Set<String> getAgents() {
-        return Collections.checkedSet(permissions.filter()
+        final Predicate<? super Triple> predicate =
+                p -> p.predicateMatches(NodeFactory.createURI(URIConstants.WEBAC_AGENT_VALUE));
+                return getStringSet(predicate);
     }
 
     @Override
     public Set<String> getAgentClasses() {
-        // TODO Auto-generated method stub
-        return null;
+        final Predicate<? super Triple> predicate =
+                p -> p.predicateMatches(NodeFactory.createURI(URIConstants.WEBAC_AGENT_CLASS_VALUE));
+                return getStringSet(predicate);
     }
 
     @Override
     public Set<URI> getModes() {
-        // TODO Auto-generated method stub
-        return null;
+        final Set<URI> theSet;
+        final Predicate<? super Triple> predicate =
+                p -> p.predicateMatches(NodeFactory.createURI(URIConstants.WEBAC_MODE_VALUE));
+
+                final RdfStream filteredTriples = permissions.filter(predicate);
+                for (final Triple t : filteredTriples) {
+                    theSet.add(URI.create(t.getObject().getURI()));
+                }
+                return theSet;
     }
 
     @Override
     public Set<String> getAccessToURIs() {
-        // TODO Auto-generated method stub
-        return null;
+        final Predicate<? super Triple> predicate =
+                p -> p.predicateMatches(NodeFactory.createURI(URIConstants.WEBAC_ACCESSTO_VALUE));
+                return getStringSet(predicate);
     }
 
     @Override
     public Set<String> getAccessToClassURIs() {
-        // TODO Auto-generated method stub
-        return null;
+        final Predicate<? super Triple> predicate =
+                p -> p.predicateMatches(NodeFactory.createURI(URIConstants.WEBAC_ACCESSTO_CLASS_VALUE));
+                return getStringSet(predicate);
     }
 
     static public WebACAuthorizationImpl getWebACAuth(final IdentifierConverter<Resource, FedoraResource> translator,
@@ -79,5 +93,14 @@ public class WebACAuthorizationImpl implements WebACAuthorization {
 
     private IdentifierConverter<Resource, FedoraResource> translator() {
         return idTranslator;
+    }
+
+    private Set<String> getStringSet(final Predicate<? super Triple> p) {
+        final Set<String> theSet;
+        final RdfStream filteredTriples = permissions.filter(p);
+        for (final Triple t : filteredTriples) {
+            theSet.add(t.getObject().getURI().toString());
+        }
+        return theSet;
     }
 }

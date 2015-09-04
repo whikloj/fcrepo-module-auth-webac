a1d3b34bc5205d7ee85ced505a410dff7ceeb01c
Jared Whiklo <jwhiklo@gmail.com>
2015-09-04 11:13:06 -0500

    Add a PUT

--- src/test/java/org/fcrepo/integration/auth/webac/WebACRecipesIT.java
+++ src/test/java/org/fcrepo/integration/auth/webac/WebACRecipesIT.java
@@ -15,20 +15,28 @@
  */
 package org.fcrepo.integration.auth.webac;
 
+import static javax.ws.rs.core.Response.Status.CREATED;
+import static org.junit.Assert.assertEquals;
 import static org.slf4j.LoggerFactory.getLogger;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.codec.binary.Base64;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpPut;
+import org.apache.http.entity.FileEntity;
 import org.apache.http.entity.InputStreamEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.apache.http.message.AbstractHttpMessage;
+import org.apache.http.util.EntityUtils;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -55,6 +63,8 @@ public class WebACRecipesIT {
     protected final PoolingHttpClientConnectionManager connectionManager = new PoolingHttpClientConnectionManager();
 
     protected static CloseableHttpClient client;
+
+    protected final ClassLoader classLoader = getClass().getClassLoader();
 
     public WebACRecipesIT() {
         connectionManager.setMaxTotal(Integer.MAX_VALUE);
@@ -89,7 +99,31 @@ public class WebACRecipesIT {
     }
 
     @Test
-    public void test() {
+    public void test() throws Exception {
+        final String objA = getRandomPid();
+        final HttpPut method = new HttpPut(serverAddress + "/" + objA);
+        final FileEntity acl =
+                new FileEntity(new File(classLoader.getResource("acls/01/acl.ttl").getFile()));
+        setAuth(method, "fedoraAdmin");
+        method.setHeader("Content-type", "text/turtle");
+        method.setEntity(acl);
+        final HttpResponse response = client.execute(method);
+        final String content = EntityUtils.toString(response.getEntity());
+        final int status = response.getStatusLine().getStatusCode();
+        assertEquals("Didn't get a CREATED response! Got content:\n" + content,
+                CREATED.getStatusCode(), status);
     }
 
+    protected static void setAuth(final AbstractHttpMessage method, final String username) {
+        final String creds = username + ":password";
+        // in test configuration we don't need real passwords
+        final String encCreds =
+                new String(Base64.encodeBase64(creds.getBytes()));
+        final String basic = "Basic " + encCreds;
+        method.setHeader("Authorization", basic);
+    }
+
+    protected static String getRandomPid() {
+        return UUID.randomUUID().toString();
+    }
 }

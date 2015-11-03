18b98a3ee2e48dc98d41024fb417c096469aa01f
Jared Whiklo <jwhiklo@gmail.com>
2015-11-02 23:41:03 -0600

    Fix the LinkHeader and update the integration tests

--- src/test/java/org/fcrepo/integration/auth/webac/WebACRecipesIT.java
+++ src/test/java/org/fcrepo/integration/auth/webac/WebACRecipesIT.java
@@ -24,6 +24,8 @@ import static org.junit.Assert.assertEquals;
 import java.io.IOException;
 import java.io.InputStream;
 
+import org.fcrepo.integration.http.api.AbstractResourceIT;
+
 import org.apache.commons.codec.binary.Base64;
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
@@ -32,10 +34,10 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPatch;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpPut;
+import org.apache.http.message.AbstractHttpMessage;
+import org.apache.http.Header;
 import org.apache.http.entity.InputStreamEntity;
 import org.apache.http.entity.StringEntity;
-import org.apache.http.message.AbstractHttpMessage;
-import org.fcrepo.integration.http.api.AbstractResourceIT;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.slf4j.Logger;
@@ -156,6 +158,7 @@ public class WebACRecipesIT extends AbstractResourceIT {
         final String testObj = ingestObj("/rest/webacl_box1");
         final String acl1 = ingestAcl("fedoraAdmin", "/acls/01/acl.ttl", "/acls/01/authorization.ttl");
         linkToAcl(testObj, acl1);
+        final String aclLink = acl1 + "; rel=\"acl\"";
 
         logger.debug("Anonymous can't read");
         final HttpGet request = getObjMethod(testObj.replace(serverAddress, ""));
@@ -163,7 +166,19 @@ public class WebACRecipesIT extends AbstractResourceIT {
 
         logger.debug("Can username 'smith123' read " + testObj);
         setAuth(request, "smith123");
-        assertEquals(HttpStatus.SC_OK, getStatus(request));
+        // assertEquals(HttpStatus.SC_OK, getStatus(request));
+
+        try (final CloseableHttpResponse response = execute(request)) {
+            assertEquals(HttpStatus.SC_OK, getStatus(response));
+            for (final Header header : response.getHeaders("Link")) {
+                logger.debug("Link header: {}", header.getValue());
+                if (header.getValue().equals(aclLink)) {
+                    return;
+                }
+            }
+            assertEquals("Missing Link Header " + aclLink.toString(), 1, 0);
+        }
+
     }
 
     @Test
